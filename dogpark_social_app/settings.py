a38cc078443c5bb408f2856bfeadea32e2b67b8d
“Nikhita <nikhitasareen@gmail.com>
2021-11-27 23:33:31 +0000

    ADDED STATIC_ROOT

--- dogpark_social_app/settings.py
+++ dogpark_social_app/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = STATIC_DIR
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js')
 # Quick-start development settings - unsuitable for production
