8c098bb4a9decfa5eea852136cda8dc918763454
“Nikhita <nikhitasareen@gmail.com>
2021-11-28 11:39:10 +0000

    undo dogpark_social_app/settings.py

--- dogpark_social_app/settings.py
+++ dogpark_social_app/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ["nameless-sea-67566.herokuapp.com", "localhost"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django_loaddata_stdin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
